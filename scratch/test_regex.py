8122bc9c92199d34bfebd55fe4bb4c78d33cf001
Julien Arino <julien.arino-UM@pm.me>
2026-06-09 15:28:44 -0500

    refactor: standardize code formatting, improve logging imports, and optimize data mapping functions

--- scratch/test_regex.py
+++
@@ -1,5 +0,0 @@
-import re
-
-dests_raw = '[[Abbotsford]],<ref name="Heintz2025"/> [[Brandon]],<ref>{{cite}}</ref> [[Cranbrook]]'
-cleaned = re.sub(r'<ref.*?</ref>', '', dests_raw, flags=re.DOTALL)
-print("Cleaned:", cleaned)
